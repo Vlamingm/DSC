5d7ce1c979bb9c4f270c42665ad4183227297076
Vlamingm <31851861+Vlamingm@users.noreply.github.com>
2017-09-29 13:27:00 +0200

    Update CreateNewOU.ps1

--- CreateNewOU.ps1
+++ CreateNewOU.ps1
@@ -1,3 +1,3 @@
-New-ADOrganizationalUnit -Name test1 -Path 'dc=testmdv, dc=nl'
-New-ADOrganizationalUnit -Name subtest1 -Path 'ou=test1, dc=testmdv, dc=nl'
+New-ADOrganizationalUnit -Name test -Path 'dc=testmdv, dc=nl'
+New-ADOrganizationalUnit -Name subtest -Path 'ou=test1, dc=testmdv, dc=nl'
 
